ed049006ab8e068b1cf25c61a183638ac1e131b2
Bali Tenaya <balitenaya666@gmail.com>
2020-11-16 18:17:50 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -46,10 +46,10 @@
         <img src="logo/bali.jpg" height="96" width="96" class="rounded-circle">
         <h5 class ="tulisan">@tenaya_bali</h5>
         <div class="button1">
-            <a type="button" href="https://www.instagram.com/tenaya_bali/" class="btn btn-outline-light btn-lg btn-block">Instagram</a>
+            <a type="button" href="https://instagram.com/tenaya_bali/" class="btn btn-outline-light btn-lg btn-block">Instagram</a>
             <a type="button" href="https://twitter.com/tenaya_bali" class="btn btn-outline-light btn-lg btn-block">Twitter</a>
-            <a type="button" href="https://web.facebook.com/balistream01" class="btn btn-outline-light btn-lg btn-block">Facebook Gamming</a>
-            <a type="button" href="https://www.youtube.com/balitenaya/" class="btn btn-outline-light btn-lg btn-block">Youtube</a>
+            <a type="button" href="https://facebook.com/balistream01" class="btn btn-outline-light btn-lg btn-block">Facebook Gamming</a>
+            <a type="button" href="https://youtube.com/balitenaya/" class="btn btn-outline-light btn-lg btn-block">Youtube</a>
             <a type="button" href="https://discord.gg/9S928uzdTd " class="btn btn-outline-light btn-lg btn-block">Discord</a>
         </div>
     </div>
