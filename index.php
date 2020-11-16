56a2553037baced3ab5ce95bc323f415d5abfbf3
Bali Tenaya <balitenaya666@gmail.com>
2020-11-16 18:02:31 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -7,54 +7,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <style>
+        @font-face{
+            font-family: "Font Digital";
+            src: url('font/Karla-Bold.ttf');
+        }
         .atas1{
-            margin: 10px;
+            margin-top: 2%;
+            font-family: "Font Digital";
+            bottom: 0px;
+        }
+        .bg{
+            background: radial-gradient(circle at top left, #505152, #666869);
+        }
+        .button1{
+            margin-top: 2%;
+        }
+        .tulisan{
+            margin-top: 1%;
+            /* font-family: Arial, Helvetica, sans-serif; */
+            color: whitesmoke;
+        }
+        .ini2{
+            font-family: monospace;
+            color: whitesmoke;
+            margin-top: 10%;
+            opacity:  0.2;
         }
     </style>
+    <link rel="icon" type="image/png" href="logo/en.png">
     <title>I DEWA GEDE BALI TENAYA</title>
 
 
 </head>
-<body>
+<body class="bg">
     <center>
-    <div class="row" style="margin-left: 15px;">
-        <div class="card atas1 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
-            <img src="logo/ig.png" class="card-img-top rounded-lg" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Instagram</h5>
-                <p class="card-text">Follow gan</p>
-                <a href="https://www.instagram.com/tenaya_bali" class="btn btn-primary">Follow</a>
-            </div>
-        </div>
-        <div class="card atas1 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
-            <img src="logo/fb.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Facebook</h5>
-                <p class="card-text">Follow gan</p>
-                <a href="https://www.facebook.com/balitenaya" class="btn btn-primary">Follow</a>
-            </div>
-        </div>
-        <div class="card atas1 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
-            <img src="logo/twit.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Twitter</h5>
-                <p class="card-text">Follow gan</p>
-                <a href="https://www.twitter.com/tenaya_bali" class="btn btn-primary">Follow</a>
-            </div>
-        </div>
-        <div class="card atas1 shadow p-3 mb-5 bg-white rounded" style="width: 18rem;">
-            <img src="logo/dc.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Discrod</h5>
-                <p class="card-text">Join gan</p>
-                <a href="https://discord.gg/9S928uzdTd" class="btn btn-primary">Join</a>
-            </div>
+    <div class="container atas1">
+        <img src="logo/bali.jpg" height="96" width="96" class="rounded-circle">
+        <h5 class ="tulisan">@tenaya_bali</h5>
+        <div class="button1">
+            <a type="button" href="https://www.instagram.com/tenaya_bali/" class="btn btn-outline-light btn-lg btn-block">Instagram</a>
+            <a type="button" href="https://wwww.twitter.com/tenaya_bali/" class="btn btn-outline-light btn-lg btn-block">Twitter</a>
+            <a type="button" href="https://web.facebook.com/balistream01" class="btn btn-outline-light btn-lg btn-block">Facebook Gamming</a>
+            <a type="button" href="https://www.youtube.com/balitenaya/" class="btn btn-outline-light btn-lg btn-block">Youtube</a>
+            <a type="button" href="https://discord.gg/9S928uzdTd " class="btn btn-outline-light btn-lg btn-block">Discord</a>
         </div>
     </div>
-
     </center>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+<footer class="ini2">
+    <center>
+        <h6>© 2020 Bali Tenaya.  All rights reserved</h6>
+    </center>
+    
+</footer>
 </html>
