6957c75f945c9b5b6be0cafb509d235820b68122
Bali Tenaya <balitenaya666@gmail.com>
2021-06-12 17:26:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
 <footer class="ini2">
   
         <h6>© 2020 Bali Tenaya.  All rights reserved</h6>
-    
+	<iframe src="https://discord.com/widget?id=772743830536847370&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
     
 </footer>
 </html>
